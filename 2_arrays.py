ce8728f3227047e3c8b5959e1823af13ed84da6b
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-09-28 15:42:29 +0300

    big O(1) for arrays

---
+++ 2_arrays.py
@@ -0,0 +1,6 @@
+# Example: Say you are storing Apple's stock prices for 5 days 
+stock_prices=[297, 330, 559, 785, 906]
+# Scenario 1: Looking for the stock price on day 3:
+day_2_stock_price=stock_prices[2]
+print(day_2_stock_price)
+# The big O complexity of looking for an array element by index is O(1) meaning, it's a constant time operation.
