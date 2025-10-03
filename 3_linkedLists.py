4ac1a5f7fe3b5d24564228c6a546e68a269bb520
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-10-03 16:36:25 +0300

    Easier implementation of the Doubly linked lists

--- 3_linkedLists.py
+++ 3_linkedLists.py
@@ -155,3 +155,42 @@ def search(head, val):
 
 
 print(search(Head, 7))
+
+# Easier implementation of Doubly linked lists
+class DoublyNode:
+    def __init__(self, val, next=None, prev=None):
+        self.val=val
+        self.next=next
+        self.prev=prev
+
+    def __str__(self):
+        return str(self.val)
+    
+head=tail=DoublyNode(1)
+
+# Display - O(n)
+def display(head):
+    curr=head
+    elements=[]
+    while curr:
+        elements.append(str(curr.val))
+        curr=curr.next
+    print("<->".join(elements))
+display(head)
+
+# Insert at beginning - O(1)
+def insert_at_beginning(head, tail, val):
+    new_node=DoublyNode(val, next=head)
+    head.prev=new_node
+    return new_node, tail
+
+head, tail=insert_at_beginning(head, tail, 3)
+display(head)
+
+# Insert at end - O(1)
+def insert_at_end(head, tail, val):
+    new_node=DoublyNode(val, prev=tail)
+    tail.next=new_node
+    return head, new_node
+head, tail=insert_at_end(head, tail, 7)
+display(head)
